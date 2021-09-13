c903a0428e91711ea5c19337804b91a67414dd7b
John Page <johncalvinpage@gmail.com>
2021-09-13 12:59:30 -0400

    Exercise 3

--- src/main/java/org/example/PrintQutoes.java
+++ src/main/java/org/example/PrintQutoes.java
@@ -10,14 +10,18 @@ public class PrintQutoes
 {
     public static void main( String[] args )
     {
+        // Gets quote from user
         System.out.println( "What is the quote? " );
 
+        //scans user input and labels it as a quote
         Scanner input = new Scanner(System.in);
-        String quote = input.nextLine();
+        String quote = input.nextLine(); // moves to next line
 
+        // Gets the speaker of the quote from the user
         System.out.print("Who said it? ");
         String speaker = input.nextLine();
 
+        // Outputs the quote with the speaker attached
         System.out.println(speaker+" says,"+"\"" +quote+ "\"");
     }
 }
